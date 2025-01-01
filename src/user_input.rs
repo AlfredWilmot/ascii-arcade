a5a990f55287729cc028da53e818b92d824516d5
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2025-01-01 17:37:48 +0000

    refactor: removed redundant return statements

--- src/user_input.rs
+++ src/user_input.rs
@@ -38,25 +38,25 @@ pub fn keyboard_control(rx: &mpsc::Receiver<termion::event::Event>) -> Cmd {
         Ok(event) => {
             match event {
                 Event::Key(key) => match key {
-                    Key::Char('d') => return Cmd::MOVE(1, 0),
-                    Key::Char('a') => return Cmd::MOVE(-1, 0),
-                    Key::Char('w') => return Cmd::MOVE(0, -1),
-                    Key::Char('s') => return Cmd::MOVE(0, 1),
-                    Key::Char('q') | Key::Esc => return Cmd::EXIT,
-                    other_key => return Cmd::DEBUG(Event::Key(other_key)),
+                    Key::Char('d') => Cmd::MOVE(1, 0),
+                    Key::Char('a') => Cmd::MOVE(-1, 0),
+                    Key::Char('w') => Cmd::MOVE(0, -1),
+                    Key::Char('s') => Cmd::MOVE(0, 1),
+                    Key::Char('q') | Key::Esc => Cmd::EXIT,
+                    other_key => Cmd::DEBUG(Event::Key(other_key)),
                 },
 
                 Event::Mouse(MouseEvent::Press(MouseButton::Left, x, y)) => {
-                    return Cmd::SPAWN(x, y)
+                    Cmd::SPAWN(x, y)
                 }
                 other_event => {
                     // other keys
-                    return Cmd::DEBUG(other_event);
+                    Cmd::DEBUG(other_event)
                 }
             }
         }
         Err(_) => {
-            return Cmd::STOP;
+            Cmd::STOP
         }
     }
 }
