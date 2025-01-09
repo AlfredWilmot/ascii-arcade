1955bbdeb5126bd8e8ad2e14c6cdef6840acefed
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2025-01-09 13:36:27 +0000

    handle collision of each entity discretely to mitigate side-effects

--- src/entity/collision.rs
+++ src/entity/collision.rs
@@ -4,7 +4,7 @@ use crate::entity::collision_geometry::{get_angle, map_angle, Square};
 use crate::entity::{Entities, Entity};
 
 pub fn resolve(entities: &mut Entities) {
-    pair_wise_comparison(entities, basic_collision_handling);
+    pair_wise_comparison(entities);
 }
 
 // -------------------------------------------------------------------------- //
@@ -14,7 +14,7 @@ pub fn resolve(entities: &mut Entities) {
 /// compares each entity on the scene against all other entities on the scene.
 /// WARNING: comparing each entity against ALL other entities on the scene
 /// is the WORST-CASE scenario (n^2)
-fn pair_wise_comparison(entities: &mut Entities, func: fn(&mut Entity, &mut Entity)) {
+fn pair_wise_comparison(entities: &mut Entities) {
     let mut entity_a: Entity;
     let mut entity_b: Entity;
 
@@ -29,10 +29,11 @@ fn pair_wise_comparison(entities: &mut Entities, func: fn(&mut Entity, &mut Enti
             // NOTE: not a fan of cloning here,
             // need to find a way to mutably access >=2 entries in the entities vector
             // this will become more important if additional unique state data is created for each entity
-            entity_a = entities[i].clone();
             entity_b = entities[j].clone();
+            entity_a = entities[i].clone();
 
-            func(&mut entity_a, &mut entity_b);
+            entity_a.handle_collision(&entity_b);
+            entity_b.handle_collision(&entity_a);
 
             entities[i] = entity_a;
             entities[j] = entity_b;
@@ -40,65 +41,63 @@ fn pair_wise_comparison(entities: &mut Entities, func: fn(&mut Entity, &mut Enti
     }
 }
 
-/// TODO: unit-test this
-pub fn basic_collision_handling(me: &mut Entity, thee: &mut Entity) {
-    // when is the projected collision going to occur?
-    // (ASSUME ON CURRENT TIME-STEP AS INTERSECTION HAS ALREADY HAPPENED)
+impl Entity {
+    /// TODO: unit-test this
+    pub fn handle_collision(&mut self, target: &Entity) {
+        // when is the projected collision going to occur?
+        // (ASSUME ON CURRENT TIME-STEP AS INTERSECTION HAS ALREADY HAPPENED)
 
-    // We're within each other's hit radii, but how should we characterize the collision?
-    let my_hitbox = Square::new(&me.pos, &me.hit_radius);
-    let thy_hitbox = Square::new(&thee.pos, &thee.hit_radius);
+        // We're within each other's hit radii, but how should we characterize the collision?
+        let my_hitbox = Square::new(&self.pos, &self.hit_radius);
+        let thy_hitbox = Square::new(&target.pos, &target.hit_radius);
 
-    if my_hitbox.intersects(&thy_hitbox) {
-        //
-        // where is the other entity relative to us?
-        let direction_of_target: f32;
-        if let Some(angle) = &get_angle(&me.pos, &thee.pos) {
-            direction_of_target = map_angle(*angle, 8);
-        } else {
-            return;
-        }
-        //
-        // we're clipping the target, so let's adjust our position...
-        let _overlap = my_hitbox.overlap_size(&thy_hitbox);
-        //
-        // are we travelling towards the other entity?
-        let direction_of_travel: f32;
-        let origin: (f32, f32) = (0.0, 0.0);
-        if let Some(angle) = &get_angle(&origin, &me.vel) {
-            direction_of_travel = map_angle(*angle, 8);
-        } else {
-            return;
-        }
-        //
-        // IF I am travelling towards the target, then consider this a COLLISION!
-        if direction_of_travel == direction_of_target {
-            let x_dir = (direction_of_travel * f32::consts::PI / 180.0).cos();
-            let y_dir = (direction_of_travel * f32::consts::PI / 180.0).sin();
-            let resultant_vels = collision_calc(
-                (x_dir * me.vel.0, y_dir * me.vel.1),
-                (x_dir * thee.vel.0, y_dir * thee.vel.1),
-                me.mass,
-                thee.mass,
-            );
-            me.target_vel(resultant_vels.0 .0, resultant_vels.0 .1);
+        if my_hitbox.intersects(&thy_hitbox) {
+            //
+            // where is the other entity relative to us?
+            let direction_of_target: f32;
+            if let Some(angle) = &get_angle(&self.pos, &target.pos) {
+                direction_of_target = map_angle(*angle, 8);
+            } else {
+                return;
+            }
+            //
+            // we're clipping the target, so let's adjust our position...
+            let _overlap = my_hitbox.overlap_size(&thy_hitbox);
+            //
+            // are we travelling towards the other entity?
+            let direction_of_travel: f32;
+            let origin: (f32, f32) = (0.0, 0.0);
+            if let Some(angle) = &get_angle(&origin, &self.vel) {
+                direction_of_travel = map_angle(*angle, 8);
+            } else {
+                return;
+            }
+            //
+            // IF I am travelling towards the target, then consider this a COLLISION!
+            if direction_of_travel == direction_of_target {
+                let x_dir = (direction_of_travel * f32::consts::PI / 180.0).cos().abs();
+                let y_dir = (direction_of_travel * f32::consts::PI / 180.0).sin().abs();
+                let resultant_vels = collision_calc(
+                    (x_dir * self.vel.0, y_dir * self.vel.1),
+                    self.mass,
+                    (x_dir * target.vel.0, y_dir * target.vel.1),
+                    target.mass,
+                );
+                self.target_vel(resultant_vels.0, resultant_vels.1);
+            }
         }
     }
 }
 
-/// calculate resultant velocity between two point masses from ...
+/// calculate resultant velocity after colliding with a target
 /// >> conservation of kinetic energy:
 /// > > 0.5*m1*v_1a^2 + 0.5*m2*v_2a^2 = 0.5*m1*v_1b^2 + 0.5*m2*v_2b^2
 /// >> conservation of momentum :
 /// > > m1*v_1a + m2*v_2a = m1*v_1b + m2*v_2b
 ///
-fn collision_calc(va: (f32, f32), vb: (f32, f32), ma: f32, mb: f32) -> ((f32, f32), (f32, f32)) {
-    let collide = |v1: f32, m1: f32, v2: f32, m2: f32| -> f32 {
-        (v1 * (m1 - m2) + 2.0 * m2 * v2) / (m1 + m2)
-    };
-    //
-    // return the resulting velocities following the collision
-    let va_new = (collide(va.0, vb.0, ma, mb), collide(va.1, vb.1, ma, mb));
-    let vb_new = (collide(vb.0, va.0, ma, mb), collide(vb.1, va.1, ma, mb));
-    (va_new, vb_new)
+fn collision_calc(va: (f32, f32), ma: f32, vb: (f32, f32), mb: f32) -> (f32, f32) {
+    (
+        (va.0 * (ma - mb) + 2.0 * mb * vb.0) / (ma + mb),
+        (va.1 * (ma - mb) + 2.0 * mb * vb.1) / (ma + mb),
+    )
 }
