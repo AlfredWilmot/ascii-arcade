4099b3921b0d6f41d7f2af0e4c38bb255992c656
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2025-01-12 13:59:29 +0000

    update unit-test

--- tests/entity_collision.rs
+++ tests/entity_collision.rs
@@ -1,23 +1,27 @@
 #[cfg(test)]
 mod tests_collision_handling {
-    use ascii_arcade::entity::{Entity, EntityType};
+    use ascii_arcade::entity::{vector::EuclidianVector, Entity, EntityType};
 
     #[test]
     fn test_force_api_doesnt_affect_motion_until_update() {
         // arrange
         let mut ent_a = Entity::new(EntityType::Player, (1.0, 1.0));
-        let expect_vel = ent_a.vel.clone();
-        let expect_acc = ent_a.acc.clone();
+        let null_vec = EuclidianVector::new(0.0, 0.0);
+        let target_vec = EuclidianVector::new(100.0, 100.0);
         //TODO// let expect_pos = ent_a.pos;
 
         // act
-        ent_a.target_vel(100.0, 100.0);
-        ent_a.target_acc(100.0, 100.0);
+        ent_a.target_vel(target_vec.x, target_vec.y);
+        ent_a.target_acc(target_vec.x, target_vec.y);
         //TODO// ent_a.target_pos(100.0, 100.0);
+        assert!(ent_a.vel == null_vec);
+        assert!(ent_a.acc == null_vec);
+        //TODO// assert!(ent_a.pos== expect_pos);
+        ent_a.update();
 
         // assert
-        assert!(ent_a.vel == expect_vel);
-        assert!(ent_a.acc == expect_acc);
-        //TODO// assert!(ent_a.pos== expect_pos);
+        println!("{:?}", ent_a.vel);
+        assert!(ent_a.vel != null_vec);
+        assert!(ent_a.acc != null_vec);
     }
 }
