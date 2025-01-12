b2ad6a02cb1cd2c69ed3b9c2556c878b03ed03d9
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2025-01-12 12:30:03 +0000

    vector from point, multiply vector by scalar

--- src/entity/vector.rs
+++ src/entity/vector.rs
@@ -1,3 +1,5 @@
+use std::{f32, ops::Mul};
+
 /// A geometric vector with a magnitude and direction
 #[derive(Clone, Debug, PartialEq)]
 pub struct EuclidianVector {
@@ -5,9 +7,60 @@ pub struct EuclidianVector {
     pub y: f32,
 }
 
+/// Returns a vector with fields multiplied by the float for '*'
+/// NOTE: the float must be on the rhs (i.e euclid_vector * f32_scalar)
+///
+/// # Example
+///
+/// ```rust
+/// use ascii_arcade::entity::vector::EuclidianVector;
+/// let a = EuclidianVector::new(2.0, -2.0);
+/// let b = a * 10.0;
+/// assert!(b.x == 20.0);
+/// assert!(b.y == -20.0);
+/// ```
+impl Mul<f32> for EuclidianVector {
+    type Output = Self;
+    fn mul(mut self, rhs: f32) -> Self {
+        self.x *= rhs;
+        self.y *= rhs;
+        self
+    }
+}
+
 impl EuclidianVector {
     pub fn new(x: f32, y: f32) -> EuclidianVector {
         EuclidianVector { x, y }
+    }
+
+    /// Returns the vector connecting two points, directed from the first to the second.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use ascii_arcade::entity::vector::EuclidianVector;
+    ///
+    /// // vector from points in Q1
+    /// let a: (f32, f32) = (2.0, 2.0);
+    /// let b: (f32, f32) = (5.0, 6.0);
+    /// let c = EuclidianVector::from(a, b);
+    /// assert!((c.x, c.y) == (3.0, 4.0));
+    /// let c = EuclidianVector::from(b, a);
+    /// assert!((c.x, c.y) == (-3.0, -4.0));
+    ///
+    /// // vector from points in Q3
+    /// let a: (f32, f32) = (-2.0, -2.0);
+    /// let b: (f32, f32) = (-5.0, -6.0);
+    /// let c = EuclidianVector::from(a, b);
+    /// assert!((c.x, c.y) == (-3.0, -4.0));
+    /// let c = EuclidianVector::from(b, a);
+    /// assert!((c.x, c.y) == (3.0, 4.0));
+    /// ```
+    pub fn from(point_a: (f32, f32), point_b: (f32, f32)) -> EuclidianVector {
+        EuclidianVector {
+            x: point_b.0 - point_a.0,
+            y: point_b.1 - point_a.1,
+        }
     }
 
     /// Returns the vector's magnitude
