077cce9ee509f477ab5aeeb904c921af45302246
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2025-01-12 10:59:29 +0000

    added dot-product calc + tests

--- src/entity/vector.rs
+++ src/entity/vector.rs
@@ -1,6 +1,4 @@
-use core::f32;
-
-/// A geometric vector with a magnitude (length) and direction
+/// A geometric vector with a magnitude and direction
 pub struct EuclidianVector {
     pub x: f32,
     pub y: f32,
@@ -18,13 +16,13 @@ impl EuclidianVector {
     /// ```rust
     /// use ascii_arcade::entity::vector::EuclidianVector;
     /// // pythagorean triples: https://en.wikipedia.org/wiki/Pythagorean_triple
-    /// assert_eq!(EuclidianVector::new(3.0,4.0).length(), 5.0);
-    /// assert_eq!(EuclidianVector::new(5.0, 12.0).length(), 13.0);
-    /// assert_eq!(EuclidianVector::new(8.0, 15.0).length(), 17.0);
-    /// assert_eq!(EuclidianVector::new(7.0, 24.0).length(), 25.0);
+    /// assert_eq!(EuclidianVector::new(3.0,4.0).magnitude(), 5.0);
+    /// assert_eq!(EuclidianVector::new(5.0, 12.0).magnitude(), 13.0);
+    /// assert_eq!(EuclidianVector::new(8.0, 15.0).magnitude(), 17.0);
+    /// assert_eq!(EuclidianVector::new(7.0, 24.0).magnitude(), 25.0);
     /// ```
-    pub fn length(&self) -> f32 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    pub fn magnitude(&self) -> f32 {
+        (self.x * self.x + self.y * self.y).sqrt()
     }
 
     /// Returns the vector's equivalent unit-vector
@@ -45,15 +43,36 @@ impl EuclidianVector {
     /// ```
     pub fn unit(&self) -> EuclidianVector {
         EuclidianVector {
-            x: self.x / self.length(),
-            y: self.y / self.length(),
+            x: self.x / self.magnitude(),
+            y: self.y / self.magnitude(),
         }
     }
 
-    // Returns a scalar that is the extent two vectors are pointing in the same direction
-    // (aka "scalar" product)
-    //
-    // https://www.techtarget.com/whatis/definition/dot-product-scalar-product
-    //pub fn dot(&self, other) -> {
-    //}
+    /// Returns a scalar that is the extent two vectors are pointing in the same direction
+    /// (aka "scalar" product)
+    ///
+    /// [What is dot-product?](https://www.techtarget.com/whatis/definition/dot-product-scalar-product)
+    ///
+    /// # Example: dot-product of unit-vectors equals their magnitude if the vectors are colinear
+    ///
+    /// ```rust
+    /// use ascii_arcade::entity::vector::EuclidianVector;
+    ///
+    /// let mut a = EuclidianVector::new(0.0, 1.0).unit();
+    /// let mut b = EuclidianVector::new(0.0, 1.0).unit();
+    /// assert_eq!(a.dot(&b), a.magnitude());
+    ///
+    /// a = EuclidianVector::new(1.0, 0.0).unit();
+    /// assert_eq!(a.dot(&b), 0.0);
+    ///
+    /// a = EuclidianVector::new(0.0, -1.0).unit();
+    /// assert_eq!(a.dot(&b), -a.magnitude());
+    ///
+    /// a = EuclidianVector::new(1.0, 1.0).unit();
+    /// b = EuclidianVector::new(1.0, 1.0).unit();
+    /// assert_eq!(a.dot(&b), a.magnitude());
+    /// ```
+    pub fn dot(&self, other: &EuclidianVector) -> f32 {
+        (self.x * other.x) + (self.y * other.y)
+    }
 }
