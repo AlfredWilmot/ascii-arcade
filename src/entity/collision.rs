9484781254e8df087eb144b9474e0f3c56272b1c
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2025-02-07 23:05:34 +0000

    wip: use overlap lengths to infer collisin orientation

--- src/entity/collision.rs
+++ src/entity/collision.rs
@@ -4,10 +4,6 @@ use super::vector::EuclidianVector;
 use crate::entity::collision_geometry::Square;
 use crate::entity::{Entities, Entity, EntityType};
 
-// -------------------------------------------------------------------------- //
-// ------------------------------ ALGORITHMS -------------------------------- //
-// -------------------------------------------------------------------------- //
-
 /// compares each entity on the scene against all other entities on the scene.
 /// WARNING: comparing each entity against ALL other entities on the scene
 /// yields the WORST-CASE compute performance (n^2) -- serves as the baseline.
@@ -27,23 +23,33 @@ pub fn pairwise(entities_then: &Entities, entities_now: &mut Entities) {
                 continue 'outer;
             }
             // fumbling in the dark; am I alone or are we just not touching?
-            if i == j || entity_under_test.overlap(entity_to_compare) == (0.0, 0.0) {
-                continue 'inner;
-            }
+            if let Some(overlap) = entity_under_test.overlap(entity_to_compare) {
+                if i == j {
+                    continue 'inner;
+                }
+                // treat encounter as grounding condition for simplicity (for now)
+                entity_under_test.grounded = true;
+                encounters += 1;
 
-            // for simplicity treating encounter as grounding condition for now
-            entity_under_test.grounded = true;
-            encounters += 1;
-            // velocity change force due to the current encounter
-            if let Some(force) = entity_under_test.try_collide(entity_to_compare) {
-                collision_force.x += force.x;
-                collision_force.y += force.y;
+                // normal force due to the current encounter
+                let you_to_me =
+                    EuclidianVector::from(entity_to_compare.pos, entity_under_test.pos).unit();
+
+                if overlap.0 >= overlap.1 {
+                    normal_force.y += you_to_me.y;
+                } else {
+                    normal_force.x += you_to_me.x;
+                }
+
+                // velocity change force due to the current encounter
+                if let Some(force) = entity_under_test.try_collide(entity_to_compare) {
+                    if overlap.0 >= overlap.1 {
+                        collision_force.y += force.y;
+                    } else {
+                        collision_force.x += force.x;
+                    }
+                }
             }
-            // normal force due to the current encounter
-            let you_to_me =
-                EuclidianVector::from(entity_to_compare.pos, entity_under_test.pos).unit();
-            normal_force.x += you_to_me.x;
-            normal_force.y += you_to_me.y;
         }
 
         if encounters > 0 {
@@ -65,21 +71,21 @@ pub fn pairwise(entities_then: &Entities, entities_now: &mut Entities) {
 impl Entity {
     /// Determine whether this entity is colliding (intersecting) with the target entity
     /// using a hitbox of some description.
-    pub fn overlap(&self, target: &Entity) -> (f32, f32) {
+    pub fn overlap(&self, target: &Entity) -> Option<(f32, f32)> {
         let my_hitbox = Square::new(&self.pos, &self.hit_radius);
         let thy_hitbox = Square::new(&target.pos, &target.hit_radius);
-        my_hitbox.overlap_size(&thy_hitbox)
+        let result = my_hitbox.overlap_size(&thy_hitbox);
+        if result == (0.0, 0.0) {
+            None
+        } else {
+            Some(result)
+        }
     }
 
     /// Determines whether this entity is colliding with some other entity, and if so,
     /// updates this entity with the forces experienced due to the change in velocity
     /// resulting from the collision.
     fn try_collide(&mut self, target: &Entity) -> Option<EuclidianVector> {
-        // are we touching?
-        if self.overlap(target) == (0.0, 0.0) {
-            return None;
-        }
-
         // where are we relative to one another?
         let me_to_you = EuclidianVector::from(self.pos, target.pos).unit();
         let you_to_me = EuclidianVector::from(target.pos, self.pos).unit();
