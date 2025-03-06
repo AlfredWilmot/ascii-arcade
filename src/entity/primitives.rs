336f26d0fe46b68c34794c0c7421cf68e42f095c
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2025-03-06 20:07:01 +0000

    adding deadzone to disregards infinitesmal overlaps

--- src/entity/primitives.rs
+++ src/entity/primitives.rs
@@ -62,16 +62,18 @@ impl Square<'_> {
     /// returns the (width, height) of the rectangle that would be formed
     /// from the overlapping area between this Square and some other Square.
     pub fn overlap(&self, other: &Square) -> Option<(f32, f32)> {
+        let deadzone: f32 = 0.01;
+
         let dx = (self.centroid.0 - other.centroid.0).abs();
         let dy = (self.centroid.1 - other.centroid.1).abs();
 
         let x_overlap = self.apothem + other.apothem - dx;
         let y_overlap = self.apothem + other.apothem - dy;
 
-        if x_overlap <= 0.0 || y_overlap <= 0.0 {
-            return None;
+        if x_overlap <= deadzone || y_overlap <= deadzone {
+            None
+        } else {
+            Some((x_overlap, y_overlap))
         }
-
-        Some((x_overlap, y_overlap))
     }
 }
