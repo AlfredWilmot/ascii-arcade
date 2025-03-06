400a8747847065fa686596ee36b7461f089be27f
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2025-03-06 20:08:25 +0000

    wip: back to fundamentals

--- src/entity/collision.rs
+++ src/entity/collision.rs
@@ -6,26 +6,20 @@ use crate::entity::{Entities, Entity};
 
 /// Applies forces generated due to contact with other entities.
 pub fn pairwise(entity: &mut Entity, other_entities: &Entities) {
+    entity.grounded = false;
+
     for other_entity in other_entities {
+        if entity.pos == other_entity.pos {
+            continue;
+        }
         // define inner hitboxes for contact force calculations
-        let hitbox_inner_a = Square::new(&entity.pos, &entity.hit_radius);
-        let hitbox_inner_b = Square::new(&other_entity.pos, &other_entity.hit_radius);
+        let hitbox_a = Square::new(&entity.pos, &entity.hit_radius);
+        let hitbox_b = Square::new(&other_entity.pos, &other_entity.hit_radius);
 
         // forces applied due to velocity change due to contact with other entity
-        if hitbox_inner_a.overlap(&hitbox_inner_b).is_some() {
+        if hitbox_a.overlap(&hitbox_b).is_some() {
             if let Some(force) = entity.collision_force(other_entity) {
-                entity.apply_force(force.x, force.y);
-            }
-        }
-
-        // define outer hitboxes for proximity force calculations
-        let outer_hit_radius_a = entity.hit_radius * 1.5;
-        let outer_hit_radius_b = other_entity.hit_radius * 1.5;
-        let hitbox_outer_a = Square::new(&entity.pos, &outer_hit_radius_a);
-        let hitbox_outer_b = Square::new(&entity.pos, &outer_hit_radius_b);
-
-        if hitbox_outer_a.overlap(&hitbox_outer_b).is_some() {
-            if let Some(force) = entity.sticking_force(other_entity) {
+                entity.grounded = true;
                 entity.apply_force(force.x, force.y);
             }
         }
@@ -33,14 +27,6 @@ pub fn pairwise(entity: &mut Entity, other_entities: &Entities) {
 }
 
 impl Entity {
-    /// Apply a displacement force based on degree of overlap, and relative position, between self and target
-    fn sticking_force(&mut self, target: &Entity) -> Option<EuclidianVector> {
-        let _me_to_you = EuclidianVector::from(self.pos, target.pos).unit();
-        let _inner_hitbox: f32 = self.hit_radius;
-
-        None
-    }
-
     /// Determines whether this entity is colliding with some other entity, and if so,
     /// updates this entity with the forces experienced due to the change in velocity
     /// resulting from the collision.
