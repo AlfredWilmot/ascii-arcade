2571ad38e1e77064205a9fabbb5d104cb177f45c
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2025-03-17 00:06:35 +0000

    amend test values to be within window boundary

--- tests/entity_motion.rs
+++ tests/entity_motion.rs
@@ -31,30 +31,34 @@ mod test_entity_motion {
     }
     #[test]
     fn test_target_pos_x_different_initial_xy() {
-        let initial: (f32, f32) = (40.0, 10.0);
-        for i in 1..50 {
-            // test from 1-to-5 in steps of 0.1 with a two-decimal resolution
-            let mut entity = Entity::new(EntityType::Npc, initial);
-            let i = 100.0 * (i as f32 * 0.1).round() / 100.0;
-            let expected = (initial.0 - i, initial.1);
-            let force = entity.target_pos(expected.0, expected.1);
-            entity.apply_force(force.clone());
-            entity.update();
-            assert_eq!(entity.pos, expected);
+        {
+            let initial: (f32, f32) = (8.0, 7.0);
+            for i in 1..50 {
+                // test from 1-to-5 in steps of 0.1 with a two-decimal resolution
+                let mut entity = Entity::new(EntityType::Npc, initial);
+                let i = 100.0 * (i as f32 * 0.1).round() / 100.0;
+                let expected = (initial.0 - i, initial.1);
+                let force = entity.target_pos(expected.0, expected.1);
+                entity.apply_force(force.clone());
+                entity.update();
+                assert_eq!(entity.pos, expected);
+            }
         }
     }
     #[test]
     fn test_target_pos_y_different_initial_xy() {
-        let initial: (f32, f32) = (10.0, 10.0);
-        for i in 1..50 {
-            // test from 1-to-5 in steps of 0.1 with a two-decimal resolution
-            let mut entity = Entity::new(EntityType::Npc, initial);
-            let i = 100.0 * (i as f32 * 0.1).round() / 100.0;
-            let expected = (initial.0, initial.1 - i);
-            let force = entity.target_pos(expected.0, expected.1);
-            entity.apply_force(force.clone());
-            entity.update();
-            assert_eq!(entity.pos, expected);
+        {
+            let initial: (f32, f32) = (8.0, 7.0);
+            for i in 1..50 {
+                // test from 1-to-5 in steps of 0.1 with a two-decimal resolution
+                let mut entity = Entity::new(EntityType::Npc, initial);
+                let i = 100.0 * (i as f32 * 0.1).round() / 100.0;
+                let expected = (initial.0, initial.1 - i);
+                let force = entity.target_pos(expected.0, expected.1);
+                entity.apply_force(force.clone());
+                entity.update();
+                assert_eq!(entity.pos, expected);
+            }
         }
     }
     #[test]
