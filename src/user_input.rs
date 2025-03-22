05a7a67e055ce6cd345a7c226307d14fb45ef0ae
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2025-03-22 23:36:41 +0000

    extracting sandbox_gam_fsm, created menu_fsm

--- src/user_input.rs
+++ src/user_input.rs
@@ -29,6 +29,8 @@ pub enum Cmd {
     MOVE(i8, i8),
     DEBUG(Event),
     SPAWN(u16, u16, EntityType),
+    SELECT,
+    RETURN,
     EXIT,
 }
 
@@ -37,29 +39,43 @@ pub enum Cmd {
 /// corresponding to the key presesd
 pub fn keyboard_control(rx: &mpsc::Receiver<termion::event::Event>) -> Cmd {
     match rx.try_recv() {
-        Ok(event) => {
-            match event {
-                Event::Key(key) => match key {
-                    Key::Char('d') => Cmd::MOVE(1, 0),
-                    Key::Char('a') => Cmd::MOVE(-1, 0),
-                    Key::Char('w') => Cmd::MOVE(0, -1),
-                    Key::Char('s') => Cmd::MOVE(0, 1),
-                    Key::Char('q') | Key::Esc => Cmd::EXIT,
-                    other_key => Cmd::DEBUG(Event::Key(other_key)),
-                },
-
-                Event::Mouse(MouseEvent::Press(MouseButton::Left, x, y)) => {
-                    Cmd::SPAWN(x, y, EntityType::Npc)
-                }
-                Event::Mouse(MouseEvent::Press(MouseButton::Right, x, y)) => {
-                    Cmd::SPAWN(x, y, EntityType::Static)
-                }
-                other_event => {
-                    // other keys
-                    Cmd::DEBUG(other_event)
-                }
-            }
-        }
+        Ok(event) => sandbox_game_fsm(event),
         Err(_) => Cmd::STOP,
+    }
+}
+
+/// Finite State Machine (FSM) for the sandbox game.
+pub fn sandbox_game_fsm(event: Event) -> Cmd {
+    match event {
+        Event::Key(key) => match key {
+            Key::Char('d') => Cmd::MOVE(1, 0),
+            Key::Char('a') => Cmd::MOVE(-1, 0),
+            Key::Char('w') => Cmd::MOVE(0, -1),
+            Key::Char('s') => Cmd::MOVE(0, 1),
+            Key::Char('q') | Key::Esc => Cmd::EXIT,
+            _ => Cmd::DEBUG(Event::Key(key)),
+        },
+        Event::Mouse(mouse) => match mouse {
+            MouseEvent::Press(MouseButton::Left, x, y) => Cmd::SPAWN(x, y, EntityType::Npc),
+            MouseEvent::Press(MouseButton::Right, x, y) => Cmd::SPAWN(x, y, EntityType::Static),
+            _ => Cmd::DEBUG(Event::Mouse(mouse)),
+        },
+        _ => Cmd::DEBUG(event),
+    }
+}
+
+/// Finite State Machine (FSM) for the main menu.
+pub fn menu_fsm(event: Event) -> Cmd {
+    match event {
+        Event::Key(key) => match key {
+            Key::Char('l') | Key::Right => Cmd::MOVE(1, 0),
+            Key::Char('h') | Key::Left => Cmd::MOVE(-1, 0),
+            Key::Char('k') | Key::Up => Cmd::MOVE(0, -1),
+            Key::Char('j') | Key::Down => Cmd::MOVE(0, 1),
+            Key::Char('q') | Key::Esc => Cmd::EXIT,
+            Key::Char('\n') => Cmd::SELECT,
+            _ => Cmd::DEBUG(Event::Key(key)),
+        },
+        _ => Cmd::DEBUG(event),
     }
 }
