243e54dae95b71a9f9ade567c3d56a744bc522b2
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2025-03-23 16:51:04 +0000

    concern separation: extract game loop into own function

--- examples/linear_collision.rs
+++ examples/linear_collision.rs
@@ -1,21 +1,20 @@
 use ascii_arcade::entity::vector::EuclidianVector;
 use ascii_arcade::entity::{update, Entities, Entity, EntityType};
-use ascii_arcade::games::process_cmds;
+use ascii_arcade::games::SandboxGame;
 use ascii_arcade::user_input::Cmd;
 use ascii_arcade::{scene, user_input};
+use std::sync::mpsc::Receiver;
 use std::thread;
 use std::time::Duration;
+use termion::event::Event;
 
 const TIME_DELTA_MS: u64 = 10;
 
-fn main() {
+fn sandbox_game(input_reader: Receiver<Event>) {
     //
     // INITIALISATION
     //
-
-    // keep the RawTerminal in scope until we exit the game
-    let terminal = scene::init();
-    let rx = user_input::create_data_channel();
+    let dt = Duration::from_millis(TIME_DELTA_MS).as_secs_f32();
 
     // keep this up-to-date on every game-loop cycle so we can query the scene by coordinates
     let mut entities_now: Entities = Vec::new();
@@ -28,7 +27,6 @@ fn main() {
     //
     // GAME LOOP
     //
-    let dt = Duration::from_millis(TIME_DELTA_MS).as_secs_f32();
     'game: loop {
         // capture the current state of the scene
         entities_then = entities_now.to_vec();
@@ -37,9 +35,13 @@ fn main() {
         let mut player = entities_now.pop().unwrap();
 
         // process user input.
-        let cmd = user_input::keyboard_control(&rx);
-        if let Cmd::EXIT = process_cmds(&mut player, &mut entities_now, cmd) {
-            break 'game;
+        if let Ok(event) = input_reader.try_recv() {
+            let cmd = user_input::sandbox_game_fsm(event);
+            if let Cmd::EXIT | Cmd::RETURN =
+                SandboxGame::process_cmds(&mut player, &mut entities_now, cmd)
+            {
+                break 'game;
+            }
         }
 
         // reinsert the player to the entity pool.
@@ -67,5 +69,11 @@ fn main() {
         scene::render(&entities_then, &entities_now);
         thread::sleep(Duration::from_secs_f32(dt));
     }
+}
+
+fn main() {
+    let terminal = scene::init();
+    let rx = user_input::create_data_channel();
+    sandbox_game(rx);
     scene::close(terminal.unwrap());
 }
