a0eb3ebf868d38b7acf142aeec6d4d54e59abe65
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2025-03-23 12:44:38 +0000

    wip: extracting function

--- examples/linear_collision.rs
+++ examples/linear_collision.rs
@@ -7,6 +7,37 @@ use std::time::Duration;
 
 const TIME_DELTA_MS: u64 = 10;
 
+
+/// Apply control signals to player, and possibly modify entity pool.
+fn process_cmds(player: &mut Entity, entities: &mut Entities, cmd: Cmd) -> Cmd {
+
+    match cmd {
+        Cmd::MOVE(x, y) => {
+            // generate movement control-force based on user-input
+            let mut move_force: EuclidianVector = if x == 0 && y != 0 {
+                player.target_vel(player.vel.x, 8.0 * y as f32)
+            } else if y == 0 && x != 0 {
+                player.target_vel(20.0 * x as f32, player.vel.y)
+            } else {
+                player.target_vel(20.0 * x as f32, 8.0 * y as f32)
+            };
+
+            // can only apply vertical control force when not free-falling
+            if !player.grounded {
+                move_force.y = 0.0;
+                move_force.x *= 0.025;
+            }
+            player.apply_force(move_force);
+        }
+        // spawn an entity of some type at some location
+        Cmd::SPAWN(x, y, id) => {
+            entities.push(Entity::new(id, (x as f32, y as f32)));
+        }
+        _ => {}
+    }
+    cmd
+}
+
 fn main() {
     //
     // INITIALISATION
@@ -32,37 +63,18 @@ fn main() {
         // capture the current state of the scene
         entities_then = entities_now.to_vec();
 
-        // create a mutable reference to the "player" entity"
-        let player = &mut entities_now[0];
+        // extract the player from the entity pool.
+        let mut player = entities_now.pop().unwrap();
 
-        // apply control signals to player
-        match user_input::keyboard_control(&rx) {
-            Cmd::MOVE(x, y) => {
-                // generate movement control-force based on user-input
-                let mut move_force: EuclidianVector = if x == 0 && y != 0 {
-                    player.target_vel(player.vel.x, 8.0 * y as f32)
-                } else if y == 0 && x != 0 {
-                    player.target_vel(20.0 * x as f32, player.vel.y)
-                } else {
-                    player.target_vel(20.0 * x as f32, 8.0 * y as f32)
-                };
-
-                // can only apply vertical control force when not free-falling
-                if !player.grounded {
-                    move_force.y = 0.0;
-                    move_force.x *= 0.025;
-                }
-                player.apply_force(move_force);
-            }
-            // spawn an entity of some type at some location
-            Cmd::SPAWN(x, y, id) => {
-                entities_now.push(Entity::new(id, (x as f32, y as f32)));
-            }
-            Cmd::EXIT => {
-                break 'game;
-            }
-            _ => {}
+        // process user input.
+        let cmd = user_input::keyboard_control(&rx);
+        match process_cmds(&mut player, &mut entities_now, cmd) {
+            Cmd::EXIT => break 'game,
+            _ => {},
         }
+
+        // reinsert the player to the entity pool.
+        entities_now.push(player);
 
         // apply global acceleration rules
         for entity in entities_now.iter_mut() {
