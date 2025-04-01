a40067ff7599bb59602cae3faf736de26ee33ef4
AlfredWilmot <>
2025-04-01 19:50:29 +0000

    update to use ref to event reader

--- examples/linear_collision.rs
+++ examples/linear_collision.rs
@@ -4,6 +4,6 @@ use ascii_arcade::{scene, user_input};
 fn main() {
     let terminal = scene::init();
     let rx = user_input::create_data_channel();
-    SandboxGame::play(rx);
+    SandboxGame::play(&rx);
     scene::close(terminal.unwrap());
 }
