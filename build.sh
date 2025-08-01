a2490347931a2b274fa78da9d227cb0f84de1077
AlfredWilmot <18071042+AlfredWilmot@users.noreply.github.com>
2025-08-02 00:13:42 +0100

    refactor: print final command, forward args

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@
 PKG="$(sed -n '/\[package\]/,/^$/{s/^name = "\(.*\)"$/\1/p}' Cargo.toml)"
 VER="$(sed -n '/\[package\]/,/^$/{s/^version = "\(.*\)"$/\1/p}' Cargo.toml)"
 
-docker build -t "${PKG}:${VER}" .
+set -x
+
+# forward any build args to docker
+docker build -t "${PKG}:${VER}" --build-arg PROJECT="${PKG}" "${@}" .
